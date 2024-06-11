116444169f015748ad3d7389d32fe0e47fdb6ad8
Elijah Apreko <elijahapreko@gmail.com>
2024-06-11 06:29:45 +0000

    add routes with navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Report from "./pages/Report";
 import Map from "./pages/Map";
