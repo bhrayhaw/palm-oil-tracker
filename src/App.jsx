f6a897cd0ab1ba238afc18c0f5059de768e71c38
Elijah Apreko <elijahapreko@gmail.com>
2024-06-11 06:29:07 +0000

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Report from "./pages/Report";
+import Map from "./pages/Map";
+import Sidebar from "./components/Sidebar";
+import TopHeader from "./components/topHeader";
+import records from "./dummyDataGhanaExtended.json"
+
+const App = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  // Calculate total area and oil extraction rate (replace with your actual logic)
+  const totalArea = records.reduce((acc, record) => acc + record.size, 0);
+  const oilExtractionRate = 85;
+
+  return (
+    <div className="flex h-screen overflow-hidden bg-gray-200">
+      <Router>
+        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        <div className="flex-1 flex flex-col overflow-hidden">
+          <TopHeader sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+          <main className="flex-1 overflow-y-auto p-4">
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <Dashboard
+                    yieldData={records}
+                    totalArea={totalArea}
+                    oilExtractionRate={oilExtractionRate}
+                  />
+                }
+              />
+              <Route path="/report" element={<Report records={records} />} />
+              <Route path="/map" element={<Map records={records} />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
