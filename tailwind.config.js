db165bc1ac3a2fe36e67132208799080543781ef
Elijah Apreko <elijahapreko@gmail.com>
2024-06-15 10:40:18 +0000

    update tailwind.config to contain font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
 
 
