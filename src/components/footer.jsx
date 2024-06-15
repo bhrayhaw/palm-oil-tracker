e1d27e74fe4835ea087e14b505181f76956fad80
Elijah Apreko <elijahapreko@gmail.com>
2024-06-15 19:42:42 +0000

    update footer for mobile screen

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="bg-white py-4 mt-auto">
       <div className="container mx-auto text-center">
-        <p className="text-sm sm:text-base md:text-lg lg:text-xl font-bold">
+        <p className="text-xs sm:text-sm md:text-base lg:text-lg font-bold">
           &copy; 2024 Palm Oil Tracker App. All rights reserved.
         </p>
       </div>
