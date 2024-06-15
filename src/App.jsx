7377f45bd422216c06617f0b274234676e4c9bba
Elijah Apreko <elijahapreko@gmail.com>
2024-06-15 17:56:14 +0000

    add dynamic imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
+import { Suspense, lazy, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import Report from "./pages/Report";
-import Map from "./pages/Map";
 import Sidebar from "./components/Sidebar";
-import TopHeader from "./components/topHeader";
+import TopHeader from "./components/TopHeader";
 import records from "./dummyDataGhanaExtended.json";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
+
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Report = lazy(() => import("./pages/Report"));
+const Map = lazy(() => import("./pages/Map"));
 
 const App = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
-  // Calculate total area and oil extraction rate (replace with your actual logic)
   const totalArea = records.reduce((acc, record) => acc + record.size, 0);
   const oilExtractionRate = 85;
 
@@ -24,22 +24,24 @@ const App = () => {
             setSidebarOpen={setSidebarOpen}
           />
           <main className="flex-1 overflow-y-auto p-4">
-            <Routes>
-              <Route
-                path="/"
-                element={
-                  <Dashboard
-                    yieldData={records}
-                    totalArea={totalArea}
-                    oilExtractionRate={oilExtractionRate}
-                  />
-                }
-              />
-              <Route path="/report" element={<Report records={records} />} />
-              <Route path="/map" element={<Map records={records} />} />
-            </Routes>
+            <Suspense fallback={<div>Loading...</div>}>
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    <Dashboard
+                      yieldData={records}
+                      totalArea={totalArea}
+                      oilExtractionRate={oilExtractionRate}
+                    />
+                  }
+                />
+                <Route path="/report" element={<Report records={records} />} />
+                <Route path="/map" element={<Map records={records} />} />
+              </Routes>
+            </Suspense>
           </main>
-        <Footer />
+          <Footer />
         </div>
       </Router>
     </div>
--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   port: 3500,
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "react-vendor": ["react", "react-dom"],
+          "chart-js": ["chart.js"],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
+  },
+});
