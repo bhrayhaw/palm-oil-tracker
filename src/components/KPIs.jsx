09ec05daf7d77fc04bdf36ebcc4a23e28dcc9027
Elijah Apreko <elijahapreko@gmail.com>
2024-06-17 08:09:49 +0000

    update responsiveness on tablet screens

--- src/components/KPIs.jsx
+++ src/components/KPIs.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import KPIItem from "./KPIItem"; // Assuming they're in the same directory
+import KPIItem from "./KPIItem";
 import {
   RiOilFill,
   RiExchangeLine,
@@ -16,45 +16,47 @@ const KPIs = ({
   totalYield,
 }) => {
   return (
-    <div className="md:grid md:grid-cols-2 lg:grid-cols-3 space-y-3 gap-6 p-6 bg-white rounded-lg shadow-lg">
-      <h2 className="sm:text-lg md:text-2xl font-semibold mb-4 text-gray-800 col-span-3">
+    <div className="bg-white rounded-lg shadow-lg p-6">
+      <h2 className="text-xl md:text-2xl font-semibold mb-4 text-gray-800">
         Key Performance Indicators
       </h2>
-      <KPIItem
-        icon={RiPlantFill}
-        title="Total Yield"
-        value={`${totalYield} tons`}
-        color="bg-blue-500"
-        textColor="text-white"
-      />
-      <KPIItem
-        icon={RiPlantLine}
-        title="Avg. Yield/Acre"
-        value={`${averageYield.toFixed(2)} tons/acre`}
-        color="bg-green-500"
-        textColor="text-white"
-      />
-      <KPIItem
-        icon={RiTimerLine}
-        title="Highest Yield"
-        value={highestYieldPeriod}
-        color="bg-yellow-500"
-        textColor="text-white"
-      />
-      <KPIItem
-        icon={RiOilFill}
-        title="Oil Extraction"
-        value={`${oilExtractionRate}%`}
-        color="bg-purple-500"
-        textColor="text-white"
-      />
-      <KPIItem
-        icon={RiExchangeLine}
-        title="Total Area"
-        value={`${totalArea.toFixed(2)} acres`}
-        color="bg-pink-500"
-        textColor="text-white"
-      />
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <KPIItem
+          icon={RiPlantFill}
+          title="Total Yield"
+          value={`${totalYield} tons`}
+          color="bg-blue-500"
+          textColor="text-white"
+        />
+        <KPIItem
+          icon={RiPlantLine}
+          title="Avg. Yield/Acre"
+          value={`${averageYield.toFixed(2)} tons/acre`}
+          color="bg-green-500"
+          textColor="text-white"
+        />
+        <KPIItem
+          icon={RiTimerLine}
+          title="Highest Yield"
+          value={highestYieldPeriod}
+          color="bg-yellow-500"
+          textColor="text-white"
+        />
+        <KPIItem
+          icon={RiOilFill}
+          title="Oil Extraction"
+          value={`${oilExtractionRate}%`}
+          color="bg-purple-500"
+          textColor="text-white"
+        />
+        <KPIItem
+          icon={RiExchangeLine}
+          title="Total Area"
+          value={`${totalArea.toFixed(2)} acres`}
+          color="bg-pink-500"
+          textColor="text-white"
+        />
+      </div>
     </div>
   );
 };
--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -4,66 +4,56 @@ import { RiPlantLine, RiPlantFill, RiTimerLine } from "react-icons/ri";
 const Summary = ({ yieldData, averageYield, totalYield }) => {
   const topYieldPeriods = yieldData
     .sort((a, b) => b.yield - a.yield)
-    .slice(0, 3);
+    .slice(0, 2);
 
   return (
-    <div className="bg-white rounded-lg shadow-md p-6 md:grid md:grid-cols-3 md:gap-6 space-y-3">
-      <h2 className="sm:text-lg md:text-2xl font-semibold mb-4 text-gray-800 col-span-3">
+    <div className="bg-white rounded-lg shadow-md p-6 space-y-4">
+      <h2 className="text-xl md:text-2xl font-semibold mb-4 text-gray-800">
         Summary
       </h2>
-      <div className="md:col-span-1">
-        <div className="flex justify-between h-full bg-blue-50 rounded-lg p-4">
-          <div className="flex items-center justify-around">
-            <div className="w-12 h-12 bg-blue-500 rounded-full flex items-center justify-center mr-4">
-              <RiPlantFill className="w-6 h-6 text-white" />
-            </div>
-            <div>
-              <p className="text-sm md:text-lg font-semibold text-gray-600">
-                Total Yield
-              </p>
-              <p className="text-sm md:text-base font-normal text-blue-800">
-                {totalYield} tons
-              </p>
-            </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="bg-blue-50 rounded-lg p-4 flex items-center">
+          <div className="w-10 h-10 md:w-12 md:h-12 bg-blue-500 rounded-full flex items-center justify-center mr-4">
+            <RiPlantFill className="w-5 h-5 md:w-6 md:h-6 text-white" />
+          </div>
+          <div>
+            <p className="text-base md:text-lg font-semibold text-gray-600">
+              Total Yield
+            </p>
+            <p className="text-sm md:text-base font-normal text-blue-800">
+              {totalYield} tons
+            </p>
           </div>
         </div>
-      </div>
-      <div className="md:col-span-1 ">
-        <div className="flex justify-between h-full bg-green-50 rounded-lg p-4">
-          <div className="flex items-center justify-around">
-            <div className="w-12 h-12 bg-green-500 rounded-full flex items-center justify-center mr-4">
-              <RiPlantLine className="w-6 h-6 text-white" />
-            </div>
-            <div>
-              <p className="text-sm md:text-lg font-semibold text-gray-600">
-                Average Yield/Acre
-              </p>
-              <p className="text-sm md:text-base font-normal text-green-800">
-                {averageYield.toFixed(2)} tons/acre
-              </p>
-            </div>
+        <div className="bg-green-50 rounded-lg p-4 flex items-center">
+          <div className="w-10 h-10 md:w-12 md:h-12 bg-green-500 rounded-full flex items-center justify-center mr-4">
+            <RiPlantLine className="w-5 h-5 md:w-6 md:h-6 text-white" />
+          </div>
+          <div>
+            <p className="text-base md:text-lg font-semibold text-gray-600">
+              Average Yield/Acre
+            </p>
+            <p className="text-sm md:text-base font-normal text-green-800">
+              {averageYield.toFixed(2)} tons/acre
+            </p>
           </div>
         </div>
-      </div>
-      <div className="md:col-span-1 justify-between">
-        <div className="flex  h-full bg-yellow-50 rounded-lg p-4">
-          <div className="flex items-center justify-around">
-            <div className="w-12 h-12 bg-yellow-500 rounded-full flex items-center justify-center mr-4">
-              <RiTimerLine className="w-6 h-6 text-white" />
-            </div>
-            <div>
-              <p className="text-sm md:text-lg font-semibold text-gray-600">
-                Top Yield Periods
+        <div className="bg-yellow-50 rounded-lg p-4 flex items-center">
+          <div className="w-10 h-10 md:w-12 md:h-12 bg-yellow-500 rounded-full flex items-center justify-center mr-4">
+            <RiTimerLine className="w-5 h-5 md:w-6 md:h-6 text-white" />
+          </div>
+          <div>
+            <p className="text-base md:text-lg font-semibold text-gray-600">
+              Top Yield Periods
+            </p>
+            {topYieldPeriods.map((yieldperiod, index) => (
+              <p
+                key={index}
+                className="text-sm md:text-base font-normal text-yellow-800"
+              >
+                {yieldperiod.date}
               </p>
-              {topYieldPeriods.map((yieldperiod, index) => (
-                <p
-                  key={index}
-                  className="text-sm md:text-base font-normal text-yellow-800"
-                >
-                  {yieldperiod.date}{" "}
-                </p>
-              ))}
-            </div>
+            ))}
           </div>
         </div>
       </div>
